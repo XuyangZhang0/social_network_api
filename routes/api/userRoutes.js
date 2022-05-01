446445c565a870c69bcd210da757058d7f92598f
Xuyang Zhang <zhangxuyang.chn@gmail.com>
2022-04-30 23:51:37 -0400

    worked on user routes, and functions

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -3,19 +3,21 @@ const {
   getUsers,
   getSingleUser,
   createUser,
+  updateUser,
   deleteUser,
   addThought,
   removeThought,
+  addFriend,
 } = require('../../controllers/userController');
 
 // /api/users
 router.route('/').get(getUsers).post(createUser);
 
 // /api/users/:userId
-router.route('/:userId').get(getSingleUser).delete(deleteUser);
+router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
 
-// /api/users/:userId/thoughts
-router.route('/:userId/thoughts').post(addThought);
+// /api/users/:userId/friends/:friendId
+router.route('/:userId/friends/:friendId').post(addFriend);
 
 // /api/users/:userId/thoughts/:thoughtId
 router.route('/:userId/thoughts/:thoughtId').delete(removeThought);
